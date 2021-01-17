41b7a9f05a5b69573f34cccc01f4b34e73102a01
sol.lee <sol.lee.0929@gmail.com>
2021-01-16 17:51:37 -0800

    💻 home page added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Login from "./pages/login";
+import Home from "./pages/home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route path="/home" component={Home} />
       </Switch>
     </Router>
   );
