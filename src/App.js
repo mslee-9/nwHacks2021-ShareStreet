487ef97081ecd08f282d8979c0a8c579bb953500
sol.lee <sol.lee.0929@gmail.com>
2021-01-16 20:21:42 -0800

    💻 new listing pages offer + request added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Home from "./pages/home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CreateNewRequest from "./pages/newrequest";
+import CreateNewOffer from "./pages/newoffer";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/home" component={Home} />
+        <Route path="/requestform" component={CreateNewRequest} />
+        <Route path="/offerform" component={CreateNewOffer} />
       </Switch>
     </Router>
   );
---
+++ src/pages/newrequest.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { NavLink } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import ItemInput from "../components/ItemInput";
+import logo from "../images/logo.png";
+import "../styles/newlisting.css";
+
+class CreateNewRequest extends React.Component {
+  render() {
+    return <h1>new request</h1>;
+  }
+}
+
+export default CreateNewRequest;
