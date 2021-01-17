d481d7a36e6ae82529d64266686581db68efb734
sol.lee <sol.lee.0929@gmail.com>
2021-01-16 17:48:53 -0800

    💻 Route in index.js changed to App

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import LogIn from "./pages/login";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LogIn />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
